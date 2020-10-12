e526736ce6673397a4ff56fc4c6191d29a3fdcbf
codecademy <ccuser@codecademy.com>
2020-10-12 16:40:22 +0000

    Add portfolio content

---
+++ app/assets/javascripts/unslider.js
@@ -0,0 +1,2 @@
+<script src="//code.jquery.com/jquery-latest.min.js"></script>
+<script src="//unslider.com/unslider.js"></script>
---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,11 @@
+class PagesController < ApplicationController
+
+  def welcome
+  
+  end
+
+  def portfolio
+
+  end
+
+end
